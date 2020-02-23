f15a358402427759920bae2157f515f2e54bd2c6
我昊 <im.hansking@gmail.com>
2020-02-24 00:19:46 +0800

    Initial Commit

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* #ifndef APP-PLUS-NVUE */
+	@import "./graceUI/graceUI.css";
+	@import "./graceUI/graceIcons.css";
+	/* #endif */
+</style>
